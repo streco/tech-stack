0029475d4a4b3d6ae3757a0041e20dff12911f28
streco <sgreco41@gmail.com>
2017-06-18 22:47:39 -0400

    Complete Redux Example

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,46 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import {
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  LayoutAnimation
+ } from 'react-native';
+import { connect } from 'react-redux';
 import CardSection from './common/CardSection';
+import * as actions from '../actions';
 
 class ListItem extends Component {
-  render() {
 
+  componentWillUpdate() {
+    LayoutAnimation.spring();
+  }
+
+  renderDescription() {
+    const { library, expanded } = this.props; //destructuring
+
+    if (expanded) {
+      return (
+        <CardSection>
+          <Text>{library.description}</Text>
+        </CardSection>
+      );
+    }
+  }
+
+
+  render() {
     const { titleStyle } = styles;
+    const { id, title } = this.props.library; //This is known as destructuring
 
     return (
-      <CardSection>
-        <Text style={titleStyle}>{this.props.library.title}</Text>
-      </CardSection>
+      <TouchableWithoutFeedback onPress={() => this.props.selectLibrary(id)}>
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>{title}</Text>
+          </CardSection>
+          {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -21,4 +51,9 @@ const styles = {
     paddingLeft: 15
   }
 };
-export default ListItem;
+
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+  return { expanded };
+};
+export default connect(mapStateToProps, actions)(ListItem);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import LibraryReducer from './LibraryReducer';
+import SelectionReducer from './SelectionReducer';
 
 
 export default combineReducers({
-  libraries: LibraryReducer //This library reducer is assigned to libraries in the state
+  libraries: LibraryReducer, //This library reducer is assigned to libraries in the state
+  selectedLibraryId: SelectionReducer
 });
